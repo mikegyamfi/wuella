fc723c4937bac712283710355b563870dc083e2e
mikegyamfi <codeage20@gmail.com>
2025-03-21 20:42:23 +0000

    change url for integration

--- intel_app/helper.py
+++ intel_app/helper.py
@@ -155,7 +155,7 @@ def send_bundle(user, receiver, bundle_amount, reference):
 
 
 def controller_send_bundle(receiver, bundle_amount, reference):
-    url = "https://testhub.geosams.com/controller/api/send_bundle/"
+    url = "https://www.geosams.com/controller/api/send_bundle/"
 
     payload = json.dumps({
         "phone_number": str(receiver),
